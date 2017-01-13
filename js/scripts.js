e8b0064af87a0da9bc87ce0f7e401209a3336523
Shane Villars <shanevillars@yahoo.com>
2017-01-13 11:45:50 -0800

    add branching functionality

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,40 @@ $("document").ready(function() {
     $(".ruby").hide();
     $(".css").hide();
 
-    var question1 = $("#question1").val();
-    var question2 = $("#question2").val();
-    var question3 = $("#question3").val();
-    var question4 = $("#question4").val();
-    var question5 = $("#question5").val();
-    var question6 = $("#question6").val();
+    var question1 = parseInt($("#question1").val());
+    var question2 = parseInt($("#question2").val());
+    var question3 = parseInt($("#question3").val());
+    var question4 = parseInt($("#question4").val());
+    var question5 = parseInt($("#question5").val());
+    var question6 = parseInt($("#question6").val());
 
-    if (question2 === "1" && question6 === "1") {
+    var ones = 0;
+    var twos = 0;
+
+    if (question1 === 1) {
+      ones += 1;
+    } if (question3 === 1) {
+      ones += 1;
+    } if (question4 === 1) {
+      ones += 1;
+    } if (question5 === 1) {
+      ones += 1;
+    } if (question1 === 2) {
+      twos += 1;
+    } if (question3 === 2) {
+      twos += 1;
+    } if (question4 === 2) {
+      twos += 1;
+    } if (question5 === 2) {
+      twos += 1;
+    }
+
+    if (question2 === 1 && question6 === 1) {
       $(".css").show();
+    } else if (ones > twos) {
+      $(".ruby").show();
+    } else {
+      $(".c").show();
     }
   })
 })
